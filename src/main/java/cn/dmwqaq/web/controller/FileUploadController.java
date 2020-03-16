359c73a09ea8a76f2ae5e21607e0f47911077937
Disperito <dmwqaq@foxmail.com>
2020-03-16 17:35:40 +0800

    删除测试方法，为md编辑器新增api

--- src/main/java/cn/dmwqaq/web/controller/FileUploadController.java
+++ src/main/java/cn/dmwqaq/web/controller/FileUploadController.java
@@ -2,35 +2,34 @@ package cn.dmwqaq.web.controller;
 
 import cn.dmwqaq.web.service.CloudObjectStorageService;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.List;
 
 @Controller
 @RequestMapping("/upload")
 public class FileUploadController {
 
+    private static Logger logger = LogManager.getLogger(FileUploadController.class);
+
     @Resource
     private CloudObjectStorageService cloudObjectStorageService;
-
-    @GetMapping("/get")
-    @ResponseBody
-    public String testGet() {
-        return "1";
-    }
 
     @PostMapping("/post")
     @ResponseBody
@@ -81,6 +80,38 @@ public class FileUploadController {
         return JSON.toJSONString(new FileUploadResponse("-1", null));
     }
 
+    @ResponseBody
+    @PostMapping(value = "/editormdImg", produces = "application/json; charset=utf-8")
+    public String editormdImageUpload(MultipartHttpServletRequest request) {
+        JSONObject object = new JSONObject();
+        object.put("success", "-1");
+        object.put("message", "上传失败！");
+        object.put("url", "");
+
+        MultipartFile multipartFile = request.getFile("image");
+        try {
+            assert multipartFile != null;
+            FileItem fileItem = ((CommonsMultipartFile) multipartFile).getFileItem();
+            String fileName = fileItem.getName();
+            File tempFile = getTempFile(fileName);
+            InputStream is = fileItem.getInputStream();
+            OutputStream os = new FileOutputStream(tempFile);
+            copyFileFromInputStream(is, os);
+
+            String fileUrl = cloudObjectStorageService.uploadFile(tempFile);
+            tempFile.delete();
+            object.put("success", "1");
+            object.put("url", fileUrl);
+            object.put("message", "上传成功!");
+            return JSON.toJSONString(object);
+        } catch (NullPointerException e) {
+            //
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return JSON.toJSONString(object);
+    }
+
     private File getTmpFilePath() {
         File file = new File("D:\\tmp");
         if (!file.exists() && !file.isDirectory()) {
@@ -88,6 +119,27 @@ public class FileUploadController {
         } else {
             return file;
         }
+    }
+
+    private void copyFileFromInputStream(InputStream is, OutputStream os) throws IOException {
+        byte[] b = new byte[1024];
+        int len;
+        while ((len = is.read(b)) != -1) {
+            os.write(b, 0, len);
+        }
+        os.close();
+        is.close();
+    }
+
+    private @NonNull
+    File getTempFile(String fileName) throws IOException {
+        File file = new File("D:\\tmp\\" + fileName);
+        if (!file.exists()) {
+            if (!file.createNewFile()) {
+                throw new RuntimeException("Mkdir failed.");
+            }
+        }
+        return file;
     }
 }
 
